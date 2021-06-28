2956a68128dc07bc1c3e39630f90e8e23d88612e
BetaHuhn <schiller@mxis.ch>
2021-06-28 16:37:30 +0200

    ✨ Use data attributes to specify opts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import Feedback, { IS_BROWSER } from './feedback'
 export default Feedback
 
-if (IS_BROWSER) {
-	(function(window) {
+const detect = () => {
+	const optsElem = document.querySelector('[data-feedback-opts]')
+
+	// If no attributes are found, assume programmatic usage and attach Feedback class to window
+	if (!optsElem) {
 		window.Feedback = Feedback
-	}(window))
+
+		return
+	}
+
+	// Parse options specified in data-feedback-opts attribute
+	const attributeValue = optsElem && optsElem.getAttribute('data-feedback-opts') || '{}'
+	const options = JSON.parse(attributeValue)
+
+	window.addEventListener('load', () => {
+		window.feedback = new Feedback(options)
+
+		// Attach the feedback button to the page
+		window.feedback.attach()
+	})
+}
+
+if (IS_BROWSER) {
+	detect()
+} else {
+	console.warn('[feedback-js] Detected environment without a `window` object')
 }
